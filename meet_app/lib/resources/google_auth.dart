2f9c9e82ecb9fab4166b7b6f72d835235342edf7
GorillaStixs <berly.kem@gmail.com>
2022-08-31 08:41:35 -0400

    create google sign in screen & google sign in auth method

---
+++ meet_app/lib/resources/google_auth.dart
@@ -0,0 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class GoogleAuth{
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firebase = FirebaseFirestore.instance;
+  Stream<User?> get authUpdates =>  _auth.authStateChanges();
+
+  Future<bool> signInWithGoogle(BuildContext context) async {
+   bool res = false;
+   try{
+     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+     final GoogleSignInAuthentication? googleAuth =  await googleUser?.authentication;
+
+     final credentials = GoogleAuthProvider.credential(
+       accessToken: googleAuth?.accessToken,
+       idToken: googleAuth?.idToken
+     );
+    UserCredential userCredential = await  _auth.signInWithCredential(credentials);
+    User? user = userCredential.user;
+
+    if(user != null){
+      if(userCredential.additionalUserInfo!.isNewUser){
+        _firebase.collection('users').doc(user.uid).set({'username': user.displayName,
+          'uid': user.uid, 'profilePhoto': user.photoURL}
+        );
+      }
+    }
+
+   } on FirebaseAuthException catch(e){
+
+   }
+  }
+}
---
+++ meet_app/lib/screens/google_sign_in_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class GoogleSiginInScreen extends StatefulWidget {
+  const GoogleSiginInScreen({Key? key}) : super(key: key);
+
+  @override
+  State<GoogleSiginInScreen> createState() => _GoogleSiginInScreenState();
+}
+
+class _GoogleSiginInScreenState extends State<GoogleSiginInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
